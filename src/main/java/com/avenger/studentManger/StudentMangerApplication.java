d6d2208d401b07ecde4148deff12c39760905f54
amitkushwahatech <amitkushwahatech@gmail.com>
2023-01-25 22:18:54 +0530

    first

---
+++ src/main/java/com/avenger/studentManger/StudentMangerApplication.java
@@ -0,0 +1,14 @@
+package com.avenger.studentManger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMangerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StudentMangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avenger/studentManger/StudentMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avenger.studentManger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
